9477d72ba5127782ca828d64f65b3611e8331bf2
douglas alves <douglasjr08@hotmail.com>
2022-05-14 17:14:05 -0300

    branch-3 criaçao de var ambiente e git ignore

---
+++ model/config/config.go
@@ -0,0 +1,48 @@
+package congif
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/subosito/gotenv"
+)
+
+var (
+	DB_PORT  = 0
+	API_PORT = 0
+	PASSWORD = 0
+	HOST     = ""
+	DB_USER  = ""
+	DB_NAME  = ""
+)
+
+func VariveisAm() {
+	var err error
+
+	err = gotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	DB_PORT, err = strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		panic(err)
+	}
+
+	PASSWORD, err = strconv.Atoi(os.Getenv("PASSWORD"))
+	if err != nil {
+		panic(err)
+	}
+
+	API_PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		panic(err)
+	}
+
+	HOST = os.Getenv("HOST")
+
+	DB_NAME = os.Getenv("DB_NAME")
+
+	DB_USER = os.Getenv("DB_USER")
+
+}

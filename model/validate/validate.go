7e25b17695bca0b4e52995fc620319f4f76dc35d
douglas alves <douglasjr08@hotmail.com>
2022-05-18 15:21:42 -0300

    branch-10 arrumando method updt e cryptografando senha

--- model/validate/validate.go
+++ model/validate/validate.go
@@ -6,52 +6,81 @@ import (
 	"regexp"
 	"strings"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func Valid(user *usuario.Usuario) error {
-	if err := validateCampo(user); err != nil {
+func Valid(user *usuario.Usuario, valida string) error {
+	if err := validateCampo(user, valida); err != nil {
 		return err
 	}
-	formate(user)
+	err := formate(valida)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func validateCampo(user *usuario.Usuario) error {
-	if user.Nome == "" {
+func validateCampo(user *usuario.Usuario, valida string) error {
+
+	if valida == "inserir" && user.Nome == "" {
 		return errors.New("campo nome tem que ser preenchido")
 	}
 
-	if user.UserName == "" {
+	if valida == "inserir" && user.UserName == "" {
 		return errors.New("campo user name tem que ser preenchido")
 	}
 
-	if user.Email == "" {
+	if valida == "inserir" && user.Email == "" {
 		return errors.New("campo email tem que ser preenchido")
 	}
 
-	err := IsEmailValid(user.Email)
-	if !err {
-		return errors.New("o email informado é invalido")
+	if valida == "inserir" {
+		err := IsEmailValid(user.Email)
+		if !err {
+			return errors.New("o email informado é invalido")
+		}
 	}
 
-	if user.Password == "" {
+	if valida == "inserir" && user.Password == "" {
 		return errors.New("campo senha tem que ser preenchido")
 	}
+
+	senha, e := Hash(user.Password)
+	if e != nil {
+		return errors.New("senha criptografada ")
+	}
+	user.Password = senha
 
 	user.DataCriacao = time.Now()
 
 	return nil
 }
 
-func formate(user *usuario.Usuario) {
+func formate(valida string) error {
+	var user usuario.Usuario
 	user.UserName = strings.TrimSpace(user.UserName)
 	user.Email = strings.TrimSpace(user.Email)
 	user.Nome = strings.TrimSpace(user.Nome)
 
+	return nil
 }
 
 func IsEmailValid(email string) bool {
 	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	return emailRegex.MatchString(email)
+}
+
+func Hash(password string) (string, error) {
+
+	senha, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", nil
+	}
+	return string(senha), nil
+}
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil // nota erro for == nil , e true e != nil é false
 }
